120352e65fe3149002b1d00a5bd54f6e57455b48
07Jerome <jeromebalagtas929@gmail.com>
2020-12-28 17:08:24 +0800

    second commit

--- js/aoretown.js
+++ js/aoretown.js
@@ -257,28 +257,28 @@ $('.quote-container').mouseup(function(){
 //   ]
 // });
 
-$(document).ready(function(){
-  $('.our_container').slick({
-      slidesToShow: 4,
-      // slidesToScroll: 1,
-      // autoplay: true,
-      // autoplaySpeed: 1500,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [{
-          breakpoint: 768,
-          settings: {
-              slidesToShow: 4
-          }
-      }, {
-          breakpoint: 520,
-          settings: {
-              slidesToShow: 3
-          }
-      }]
-  });
-});
+// 
+  // $('.our_container ').slick({
+  //     slidesToShow: 5,
+  //     // slidesToScroll: 1,
+  //     // autoplay: true,
+  //     // autoplaySpeed: 1500,
+  //     arrows: false,
+  //     dots: false,
+  //     pauseOnHover: false,
+  //     responsive: [{
+  //         breakpoint: 768,
+  //         settings: {
+  //             slidesToShow: 4
+  //         }
+  //     }, {
+  //         breakpoint: 520,
+  //         settings: {
+  //             slidesToShow: 3
+  //         }
+  //     }]
+  // });
+
 
 
 
